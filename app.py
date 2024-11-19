511dea5cf5d9e7e75bde37cbc842d9b9a6ea5802
Adebayo Olaonipekun <pekunmi@live.com>
2024-11-19 14:28:31 +0000

    Commented out fastapi so we dont have issues when we deploy

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import requests
 import joblib
 import numpy as np
 
+# Load model
+model = joblib.load("model/model.pkl")
+
 # app title and description
 st.title("Power Output Prediction")
 st.write("Enter the values for the following features to predict power output (PE).")
@@ -14,21 +17,27 @@ ambient_pressure = st.number_input("Ambient Pressure (AP)", value=1000.0)
 relative_humidity = st.number_input("Relative Humidity (RH)", value=75.0)
 
 if st.button("Predict"):
+    input_data = np.array([[
+        ambient_temperature, exhaust_vacuum, ambient_pressure, relative_humidity
+    ]])
+    prediction = model.predict(input_data)
+    st.write(f"Predicted Power Output (PE): {prediction[0]}")
+
     # prepare data
-    input_data = {
-        "ambient_temperature": ambient_temperature,
-        "exhaust_vacuum": exhaust_vacuum,
-        "ambient_pressure": ambient_pressure,
-        "relative_humidity": relative_humidity
-    }
+    # input_data = {
+    #     "ambient_temperature": ambient_temperature,
+    #     "exhaust_vacuum": exhaust_vacuum,
+    #     "ambient_pressure": ambient_pressure,
+    #     "relative_humidity": relative_humidity
+    # }
 
     # make api call
-    response = requests.post("http://0.0.0.0:8000/predict", json=input_data)
+    # response = requests.post("http://0.0.0.0:8000/predict", json=input_data)
 
-    if response.status_code == 200:
-        prediction = response.json()["prediction"]
-        st.write(f"Predicted Power Output (PE): {prediction}")
-    else:
-        st.write("Error in prediction. Please try again.")
+    # if response.status_code == 200:
+    #     prediction = response.json()["prediction"]
+    #     st.write(f"Predicted Power Output (PE): {prediction}")
+    # else:
+    #     st.write("Error in prediction. Please try again.")
 
 
